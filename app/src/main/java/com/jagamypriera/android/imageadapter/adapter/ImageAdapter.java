fc8a849f19b81005b72d2438dbb6ae7be29f7b1a
Jaga My Priera <mj.jaga@gmail.com>
2017-06-02 14:15:27 +0700

    add loading indicator

--- app/src/main/java/com/jagamypriera/android/imageadapter/adapter/ImageAdapter.java
+++ app/src/main/java/com/jagamypriera/android/imageadapter/adapter/ImageAdapter.java
@@ -2,16 +2,19 @@ package com.jagamypriera.android.imageadapter.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.target.ViewTarget;
 import com.jagamypriera.android.imageadapter.R;
 import com.jagamypriera.android.imageadapter.activity.ImageViewerActivity;
@@ -68,11 +71,19 @@ public class ImageAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         View itemView = mLayoutInflater.inflate(R.layout.layout_image_view, container, false);
         final ImageView imageView = (ImageView) itemView.findViewById(R.id.img_image);
+        final ProgressBar progressBar = (ProgressBar) itemView.findViewById(R.id.progress);
         Glide.with(mContext).load(images.get(position)).fitCenter().dontAnimate().into(new ViewTarget<ImageView, GlideDrawable>(imageView) {
             @Override
             public void onResourceReady(GlideDrawable resource, GlideAnimation anim) {
                 this.view.setImageDrawable(resource);
+                progressBar.setVisibility(View.GONE);
                 if (zoomable)mAttacher = new PhotoViewAttacher(imageView);
+            }
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                super.onLoadFailed(e, errorDrawable);
+                progressBar.setVisibility(View.GONE);
             }
         });
         container.addView(itemView);
